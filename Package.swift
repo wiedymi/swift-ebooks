94b87eadb112de67496e6cabd2aa08d6bc1a09f1
Wiedy Mi <42713027+wiedymi@users.noreply.github.com>
2026-09-25 19:58:28 +0800

    Use upstream public ZIPFoundation

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
         .executable(name: "BookKitExample", targets: ["BookKitExample"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/vivy-company/ZIPFoundation.git", from: "0.9.19"),
+        .package(url: "https://github.com/weichsel/ZIPFoundation.git", from: "0.9.19"),
         .package(url: "https://github.com/scinfu/SwiftSoup.git", from: "2.6.0"),
     ],
     targets: [
